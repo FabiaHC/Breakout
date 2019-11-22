8f49f04cc65d15a282aaeee952166e0b21732adf
Fabian <megaweepinbell@gmail.com>
2019-11-22 19:31:27 +0000

    Add initial Pygame logic code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import pygame
+import random
+
 def main():
-    pass
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    done = False
+    pygame.display.set_caption('Breakout!')
+    clock = pygame.time.Clock()
+
+    while not done:
+        pygame.event.pump()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
