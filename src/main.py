472650bad3aabebf8bc3423c24991e5058a01ee6
Fabian <megaweepinbell@gmail.com>
2019-11-23 15:51:28 +0000

    Add TextBox class and buttons to MenuScene class

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import pygame
 import random
 import scene
+from scene import TextBox
 
 class MenuScene(scene.Scene):
-    def init(self):
-        print("Menu Setup")
+    def __init__(self, screen):
+        self.__screen = screen
+        x, y = self.__screen.get_size()
+        x //= 100 #One percent of pixels in the x axis
+        y //= 100 #One percent of pixels in the y axis
+        self.__buttons = {}
+        self.__buttons["start"] = TextBox(20, [50, 15], "Start", (x, y))
 
     def loop(self):
         return False
--- src/scene.py
+++ src/scene.py
@@ -1,3 +1,5 @@
+import pygame
+
 class SceneManager():
     def __init__(self, initialSceneName, initialScene):
         self.__currentScene = initialSceneName
@@ -12,12 +14,28 @@ class SceneManager():
         self.__currentScene = sceneName
 
 class Scene():
-    def __init__(self, screen):
-        self.__screen = screen
-
     def init(self):
         pass
 
     def loop(self):
         done = False
         return done
+
+class TextBox():
+    def __init__(self, size, pos, text, screenPercentage):
+        font = pygame.font.Font('assets/PressStart2P.ttf', size*screenPercentage[1])
+        self.__textSurface = font.render(text, True, (0,0,0))
+        self.__textRect = self.__textSurface.get_rect()
+        rectSize = self.__textSurface.get_size()
+        pos[0] = pos[0] * screenPercentage[0] - rectSize[0] // 2
+        pos[1] = pos[1] * screenPercentage[1] - rectSize[1] // 2
+        self.__textRect.move_ip(pos[0], pos[1])
+        self.__pos = pos
+
+    def blit(self, screen, boxBorder):
+        if boxBorder:
+            pygame.draw.rect(screen, (0, 0, 0), self.__textRect, 3)
+        screen.blit(self.__textSurface, self.__pos)
+
+    def getRect(self):
+        return self.__textRect
